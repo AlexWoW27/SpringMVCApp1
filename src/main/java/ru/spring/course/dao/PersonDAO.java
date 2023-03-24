329f30111a09d7cab72bbfb022b798e641525f18
Aleksei Stepanov <twomix1994@gmail.com>
2023-03-25 00:23:33 +0300

    update

--- src/main/java/ru/spring/course/dao/PersonDAO.java
+++ src/main/java/ru/spring/course/dao/PersonDAO.java
@@ -1,2 +1,44 @@
-package ru.spring.course.dao;public class PersonDao {
+package ru.spring.course.dao;
+
+import org.springframework.stereotype.Component;
+import ru.spring.course.models.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+    private static int PEOPLE_COUNT;
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+        people.add(new Person(++PEOPLE_COUNT,"Tom"));
+        people.add(new Person(++PEOPLE_COUNT,"Bob"));
+        people.add(new Person(++PEOPLE_COUNT,"Mike"));
+        people.add(new Person(++PEOPLE_COUNT,"Katy"));
+    }
+
+    public List<Person> index(){
+        return people;
+    }
+
+    public Person show(int id){
+        return people.stream().filter(p->p.getId()==id).findAny().orElse(null);
+    }
+
+    public void save(Person person){
+        person.setId(++PEOPLE_COUNT);
+        people.add(person);
+    }
+
+    public void update(int id, Person updayePerson){
+        Person personToBeUpdated = show(id);
+
+        personToBeUpdated.setName(updayePerson.getName());
+
+    }
+    public void delete(int id){
+        people.removeIf(p -> p.getId() == id);
+    }
 }
